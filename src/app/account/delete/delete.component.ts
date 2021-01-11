ded2f37beb86a35db97ba28ecb0287ed22555119
duc le hoang <duclehoang@ducs-MacBook-Pro.local>
2021-01-12 03:44:05 +0700

    fix src

--- src/app/account/delete/delete.component.ts
+++ src/app/account/delete/delete.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { AccountService } from 'src/app/core/services/account.service';
+import { AccountService } from 'app/core/services/account.service';
 
 @Component({
   selector: 'app-delete',
