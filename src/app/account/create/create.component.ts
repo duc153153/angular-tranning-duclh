17a4d58446e1e5e884f4549b490beae8f5ba1530
duc le hoang <duclehoang@ducs-MacBook-Pro.local>
2021-01-12 03:45:44 +0700

    fix src

--- src/app/account/create/create.component.ts
+++ src/app/account/create/create.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { AccountService } from 'app/core/services/account.service';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-create',
