04915d7db4c8aaa9668792fbc245d7091f896a87
duc le hoang <duclehoang@ducs-MacBook-Pro.local>
2021-01-12 11:22:05 +0700

    fix src

--- src/app/account/update/update.component.ts
+++ src/app/account/update/update.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { AccountService } from 'src/app/core/services/account.service';
+import { AccountService } from '../../core/services/account.service';
 import { CreateComponent } from '../create/create.component';
 
 @Component({
